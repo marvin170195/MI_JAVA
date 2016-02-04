5153b0e3c71f2864dac0d63dc49e6ce74b662a35
Marvin Iberico <marviniberico@gmail.com>
2016-02-04 09:38:12 -0500

    Actualizando

---
+++ CLASE02/AprendiendoJava/src/pe/egcc/app/model/Producto.java
@@ -0,0 +1,55 @@
+package pe.egcc.app.model;
+
+/**
+ *
+ * @author Gustavo Coronel
+ */
+public class Producto {
+
+  private String nombre;
+  private int stock;
+  private double precio;
+  private boolean activo;
+
+  public Producto() {
+    this.nombre = "Curso Java";
+    this.precio = 300.0;
+    this.stock = 20;
+    this.activo = true;
+  }
+  
+  
+    
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public int getStock() {
+    return stock;
+  }
+
+  public void setStock(int stock) {
+    this.stock = stock;
+  }
+
+  public double getPrecio() {
+    return precio;
+  }
+
+  public void setPrecio(double precio) {
+    this.precio = precio;
+  }
+
+  public boolean isActivo() {
+    return activo;
+  }
+
+  public void setActivo(boolean activo) {
+    this.activo = activo;
+  }
+
+}
